18697202510aa35b858860b3fe278597b92df854
MGZeroes <MGZeroes@users.noreply.source.mgzeroes.com>
2022-04-26 19:59:52 +0000

    Implement Initialization

--- chip8.c
+++ chip8.c
@@ -1,5 +1,60 @@
 #include "chip8.h"
 
+/**
+ * initilize the system to its startup state
+ * @param chip8 
+ */
+void initializeSystem(chip8 *chip8)
+{
+	chip8->isRunning = TRUE;
+	chip8->drawingScreen = FALSE;
+	chip8->isPaused = FALSE;
+
+	chip8->pc = PROGRAM_START;
+	chip8->opcode = 0;
+	chip8->sp = 0;
+	chip8->I = 0;
+
+	// clear display (memory)
+	for (int i = 0; i < VIDEO_HEIGHT; i++)
+	{
+		for (int j = 0; j < VIDEO_WIDTH; j++)
+		{
+			chip8->gfx[i][j] = 0;
+		}
+	}
+
+	// clear ram
+	for (int i = 0; i < RAM_SIZE; i++)
+	{
+		chip8->ram[i] = 0;
+	}
+
+	// clear registers
+	for (int i = 0; i < REGISTER_SIZE; i++)
+	{
+		chip8->V[i] = 0;
+	}
+
+	// load fontset into memory
+	for(int i = 0; i < FONTSET_SIZE; i++)
+	{
+		chip8->ram[i] = FONTSET[i];
+	}
+	
+	// set timers to 0
+	chip8->delayTimer = 0;
+	chip8->soundTimer = 0;
+
+	// keyboard setup
+	for (int i = 0; i < NUMBER_KEYS; i++)
+	{
+		chip8->keyboard[i] = FALSE;
+	}
+
+	chip8->keyPressed = FALSE;
+}
+
 
 /**
  * fetches the current opcode (opcode is 2 bytes)
